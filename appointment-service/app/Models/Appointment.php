02c13bf68d1e288ded68d6e8a50c06ebc8f91eae
faizaclara <faizaclara23@gmail.com>
2026-06-21 02:55:56 +0700

    feat(appointment): complete patient and pharmacy end-to-end integration

--- appointment-service/app/Models/Appointment.php
+++ appointment-service/app/Models/Appointment.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     protected $fillable = [
+        'patient_id',
         'patient_name',
         'doctor_name',
         'specialization',
